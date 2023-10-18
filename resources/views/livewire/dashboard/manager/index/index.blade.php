ad81369ee924746db6755d92bd252d2f868bc739
Ali Golzar <agt.ali.gr@gmail.com>
2023-10-18 18:02:23 +0330

    WS

--- resources/views/livewire/dashboard/manager/index/index.blade.php
+++ resources/views/livewire/dashboard/manager/index/index.blade.php
@@ -1,5 +1,5 @@
 <div class="container-xxl">
-    <a wire:click="udp" class="btn btn-primary">DO</a>
+    <a onclick="test()" class="btn btn-primary">DO</a>
 </div>
 
 @section('title')
@@ -8,3 +8,24 @@
 @section('description')
     پیشخان کنترل پنل
 @endsection
+@push('scripts')
+    <script>
+        var test = null;
+        var connection = new WebSocket('ws://127.0.0.1:8080');
+        connection.onopen = function () {
+            var i = 0;
+            while (true) {
+                i++;
+                if(i == 100000) {
+                    connection.send(Date.now());
+                    i = 0;
+                }
+            }
+         test = function () {
+            console.log("sdfsdfd");
+
+                connection.send(Date.now()); // Send the message 'Ping' to the server
+
+        }};
+    </script>
+@endpush

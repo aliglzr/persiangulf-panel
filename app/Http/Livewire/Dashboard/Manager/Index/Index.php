8d7c8201d6b8757433b1044ecc73b72961fd4cab
Ali Golzar <agt.ali.gr@gmail.com>
2023-11-11 09:54:23 +0330

    Changed codes

--- app/Http/Livewire/Dashboard/Manager/Index/Index.php
+++ app/Http/Livewire/Dashboard/Manager/Index/Index.php
@@ -7,6 +7,15 @@ use Livewire\Component;
 class Index extends Component
 {
     public $code;
+    public $lr;
+    public $ud;
+    public $f;
+    public $b;
+    private $bldc1;
+    private $bldc2;
+    private $bldc3;
+    private $bldc4;
+    public float $micro = 0;
     public function getListeners(): array
     {
         return [
@@ -16,18 +25,73 @@ class Index extends Component
     }
     public function mount()
     {
+        $this->micro = 0;
+    }
+
+    public function command($lr, $ud,$f,$b, $gear, $micro) {
+        if($this->micro == 0) {
+            $this->micro = $micro;
+        }
+        if($micro - $this->micro > 100) {
+            $this->micro = $micro;
+        } else {
+            return;
+        }
+
+        // LeftRight $lr
+        // UpDown $ud
+        // Forward $f
+        // Backward $b
+        if(true) {
+            $server_ip   = '127.0.0.1';
+            $server_port = 8080;
+            $beat_period = 5;
+            $maximum = 0;
+            if($gear == 1) {
+                $maximum = 24;
+            } else if($gear == 2) {
+                $maximum = 96;
+            } else if($gear == 3) {
+                $maximum = 168;
+            } else if($gear == 4) {
+                $maximum = 240;
+            }
+            $bldc2 = 0;
+            $bldc4 = 0;
+            $bldc1 = $maximum * $b;
+            $bldc3 = $maximum * $b;
+            $bnegative = 1;
+            if($b < 0.001) {
+                $bldc1 = $maximum * $f * -1;
+                $bldc3 = $maximum * $f * -1;
+                $bnegative = -1;
+            }
+
+            $angleForce = 0.3 * $bldc1;
+            if($lr > 0) {
+                $bldc1 += $angleForce * $lr * $bnegative;
+                $bldc3 -= $angleForce * $lr * $bnegative;
+            } else if($lr < 0) {
+                $bldc1 += $angleForce * $lr * $bnegative;
+                $bldc3 -= $angleForce * $lr * $bnegative;
+            }
+
+            $bldc2 = $maximum * -1 * $ud;
+            $bldc4 = $maximum * -1 * $ud;
+//            dd($bldc1, $bldc3, $bldc2, $bldc4);
+//            $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => "bldc1:".$bldc1]);
+
+            $message     = '{"rid":'.rand(1000,9999).',"bldc1":'.$bldc1.',"bldc2":'.$bldc2.','.'"bldc3":'.$bldc3.','.'"bldc4":'.$bldc4.'}';
+            if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
+                socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
+            } else {
+                echo ("can't create socketn");
+            }
+        }
+
     }
 
     public function udp() {
-        $server_ip   = '127.0.0.1';
-        $server_port = 8080;
-        $beat_period = 5;
-        $message     = microtime(true);
-        if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
-                socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
-        } else {
-            echo ("can't create socketn");
-        }
     }
 
 
--- resources/views/livewire/dashboard/manager/index/index.blade.php
+++ resources/views/livewire/dashboard/manager/index/index.blade.php
@@ -21,11 +21,144 @@
                     i = 0;
                 }
             }
-         test = function () {
-            console.log("sdfsdfd");
+            test = function () {
+                console.log("sdfsdfd");
 
                 connection.send(Date.now()); // Send the message 'Ping' to the server
 
-        }};
+            }};
+        const canvas = document.getElementById("gameArea");
+        const ctx = canvas.getContext("2d");
+
+        let playerWidthAndHeight = 0;
+        let playerX = 0;
+        let playerY = 0;
+        let playerColor = "orange";
+        let gear = 1;
+        let velocity = 0;
+        let lr = 0;
+        let ud = 0;
+        let f = 0;
+        let b = 0;
+
+        let controllerIndex = null;
+        let leftPressed = false;
+        let rightPressed = false;
+        let upPressed = false;
+        let downPressed = false;
+
+        let bluePressed = false;
+        let orangePressed = false;
+        let redPressed = false;
+        let greenPressed = false;
+
+        function setupCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            playerWidthAndHeight = canvas.width * 0.1;
+            velocity = 0.01;
+
+            playerX = (canvas.width - playerWidthAndHeight) / 2;
+            playerY = (canvas.height - playerWidthAndHeight) / 2;
+        }
+
+        setupCanvas();
+
+        window.addEventListener("resize", setupCanvas);
+        window.addEventListener("gamepadconnected", (event) => {
+            controllerIndex = event.gamepad.index;
+            console.log("connected");
+        });
+
+        window.addEventListener("gamepaddisconnected", (event) => {
+            console.log("disconnected");
+            controllerIndex = null;
+        });
+
+        function clearScreen() {
+            ctx.fillStyle = "#333331";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
+        function drawPlayer() {
+            ctx.fillStyle = playerColor;
+            ctx.fillRect(playerX, playerY, playerWidthAndHeight, playerWidthAndHeight);
+        }
+
+        function controllerInput() {
+            if (controllerIndex !== null) {
+                const gamepad = navigator.getGamepads()[controllerIndex];
+
+                const buttons = gamepad.buttons;
+                upPressed = buttons[12].pressed;
+                downPressed = buttons[13].pressed;
+                leftPressed = buttons[14].pressed;
+                rightPressed = buttons[15].pressed;
+
+                const stickDeadZone = 0.4;
+                const leftRightValue = gamepad.axes[0];
+                const forwardForce = gamepad.buttons[7].value;
+                const backwardForce = gamepad.buttons[6].value;
+                console.log("leftRightValue:" + leftRightValue);
+                if (leftRightValue >= stickDeadZone) {
+                    rightPressed = true;
+                } else if (leftRightValue <= -stickDeadZone) {
+                    leftPressed = true;
+                }
+
+                const upDownValue = gamepad.axes[1];
+
+                @this.command(leftRightValue, upDownValue, gamepad.buttons[7].value, gamepad.buttons[6].value,gear, Date.now());
+
+
+                if (upDownValue >= stickDeadZone) {
+                    downPressed = true;
+                } else if (upDownValue <= -stickDeadZone) {
+                    upPressed = true;
+                }
+
+                redPressed = buttons[0].pressed;
+                bluePressed = buttons[1].pressed;
+                orangePressed = buttons[2].pressed;
+                greenPressed = buttons[3].pressed;
+
+                if (bluePressed) {
+                    greenPressed = false;
+                    redPressed = false;
+                    orangePressed = false;
+                    gear = 1;
+                } else if (greenPressed) {
+                    bluePressed = false;
+                    redPressed = false;
+                    orangePressed = false;
+                    gear = 4;
+                } else if (redPressed) {
+                    greenPressed = false;
+                    bluePressed = false;
+                    orangePressed = false;
+                    gear = 3;
+                } else if (orangePressed) {
+                    greenPressed = false;
+                    redPressed = false;
+                    bluePressed = false;
+                    gear = 2;
+                }
+            }
+        }
+
+        function changePlayerColor() {
+        }
+
+        function updatePlayer() {
+            changePlayerColor();
+        }
+
+        function gameLoop() {
+            controllerInput();
+            updatePlayer();
+            requestAnimationFrame(gameLoop);
+        }
+
+        gameLoop();
     </script>
 @endpush
